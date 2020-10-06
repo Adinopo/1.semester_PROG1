7131577a8ff3dcb19725326dfcdece82d767804a
Adinopo <plavocka007@gmail.com>
2020-10-06 14:27:05 +0200

    Adding new source code...

---
+++ Cvičenia z kniky PDF.py
@@ -0,0 +1,11 @@
+var = 2999
+var3 = 0
+
+var = input("Enter value: ")
+
+print("Zadal si hodnotu: \n", str(var))
+
+while input(str(var3)) != "q":
+    print("Zadaj si : ",var3)
+
+#   Prevod casu na min a sec spraviť:
