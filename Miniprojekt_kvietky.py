9909e50b31456d526bd887552ad1bfac5cbf9f18
Adinopo <plavocka007@gmail.com>
2020-10-13 16:20:12 +0200

    Adding final Project-flowers

--- Miniprojekt_kvietky.py
+++ Miniprojekt_kvietky.py
@@ -3,34 +3,63 @@ import turtle
 
 turtle.delay(0)
 
-def polupen(uhol,strana,velkost):          # Uhol a Strana spolu suvisia
-    for i in range(strana):
+def polupen(uhol, pocet, velkost):                        # Uhol a Strana spolu suvisia
+    for i in range(pocet):
         turtle.fd(velkost)
         turtle.left(uhol)
 
-def lupen(uhol, strana, velkost):
-    for i in range(2):
-        polupen(uhol, strana, velkost)
-        turtle.left(180-uhol*strana)
 
-def kvet(uhol,strana,velkost,pocet_lup):
+def lupen(uhol, pocet, velkost):
+    for i in range(2):
+        polupen(uhol, pocet, velkost)
+        turtle.left(180 - uhol * pocet)
+
+
+def list_l(uhol, pocet, velkost, uhol_listu):
+    turtle.seth(uhol_listu)                                # Uhol prvého listu od zeme
+    for i in range(2):
+        polupen(uhol, pocet, velkost)
+        turtle.left(180 - uhol * pocet)
+
+
+def list_p(uhol, pocet, velkost, uhol_listu):
+    turtle.seth(180-uhol_listu)                            # Uhol druheho listu od zeme, musí byť rovnaký ako prvý
+    for i in range(2):
+        for j in range(pocet):
+            turtle.fd(velkost)
+            turtle.right(uhol)
+        turtle.right(180 - uhol * pocet)
+
+
+def polupen_lavotocivy(uhol, pocet, dlzka):
+    for i in range(pocet):
+        turtle.forward(dlzka)
+        turtle.right(uhol)
+
+
+def stonka(polomer, uhol):                                  # Arc = oblúk
+    arc_length = 2 * math.pi * polomer                      # s týmto uhol/360 to len zmenší dlzku stonky
+    n = int(arc_length / 4)
+    print(n)
+    step_length = arc_length / n
+    print(step_length)
+    step_angle = float(uhol) / n
+    print(step_angle)
+
+    turtle.seth(180-(180-uhol)/2)
+    polupen_lavotocivy(step_angle,n,step_length)
+
+
+def kvet(uhol_lupen, uhol_list, pocet_lupen, pocet_list, velkost, pocet_lup, uhol_listu):        # uhol_lupen/list = tučnota lista, velkost = dlzka listov, pocet_list/lupen = tučnota listu
+    list_l(uhol_list, pocet_list, velkost, uhol_listu)
+    list_p(uhol_list, pocet_list, velkost, uhol_listu)
+    stonka(60,90)
     for i in range(pocet_lup):
-        lupen(uhol, strana, velkost)
+        lupen(uhol_lupen, pocet_lupen, velkost)
         turtle.left(360/pocet_lup)
 
-def arc(polomer, uhol):                      # Arc = oblúk
-    arc_length = 2 * math.pi * polomer
-    n = int(arc_length / 4)
-    step_length = arc_length / n
-    step_angle = float(uhol) / n
-
-    turtle.left(step_angle/2)
-    turtle.seth(50)
-    polupen(step_angle,n,step_length)
-    turtle.right(step_length/2)
 
 
-arc(40,80)                              # prvý argumet je POLOMER, druhý je ako velmi zaoblená bude
-#kvet(4,10,20,12)
+kvet(8,6,10,15,9,8,30)
 
 turtle.exitonclick()
